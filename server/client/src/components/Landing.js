2ef4933214fcfe718a426cfea125639f3ab01ea8
Vid Mishra <vidyanandcdac13@gmail.com>
2019-02-21 01:32:15 +0530

    Initial Commit

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                FeedbApp!
+            </h1>
+            Collect feedback from your users
+        </div>
+    );
+};
+
+export default Landing;
---
+++ server/client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: "Survey Title", name: "title", errorMsg: "Provide title!" },
+    { label: "Subject Line", name: "subject", errorMsg: "Provide subject!" },
+    { label: "Email Body", name: "body", errorMsg: "Provide email's body!" },
+    { label: "Recipient List", name: "recipients", errorMsg: "Provide email ID!" }
+];
